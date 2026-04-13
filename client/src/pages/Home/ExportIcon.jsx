4c2f282c99911c28a4b76e9319da751fe6c753f1
AbdulMalik <>
2026-04-13 16:23:18 +0300

    fixed title & favicon

---
+++ client/src/pages/Home/ExportIcon.jsx
@@ -0,0 +1,9 @@
+import { Sparkles } from "lucide-react";
+
+export default function ExportIcon() {
+    return (
+        <div id="icon">
+            <Sparkles size={128} />
+        </div>
+    );
+}
